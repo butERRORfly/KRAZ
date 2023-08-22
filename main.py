b1d699f80eaf6ac861eef3d5c2e7374da5443d3d
butERRORfly <67109808+butERRORfly@users.noreply.github.com>
2023-08-22 20:02:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import openpyxl
+import csv
 
+choose_file = input('Введи имя файла без .csv: ')
+
+# Конвертация csv в excel
+wb = openpyxl.Workbook()
+ws = wb.active
+with open(f'{choose_file}.csv') as f:
+    reader = csv.reader(f, delimiter=';')
+    for row in reader:
+        ws.append(row)
+wb.save(f'{choose_file}.xlsx')
 
 # Получаем данные из файла
-wb = openpyxl.load_workbook('DAM_KEP.xlsx')
+wb = openpyxl.load_workbook(f'{choose_file}.xlsx')
 
 # Получаем доступ к активному листу
 ws = wb.active
 
 # Фильтрация по ключам
+
 # Создаем новые листы
 good_sheet = wb.create_sheet('Good')
 bad_sheet = wb.create_sheet('Bad')
@@ -34,18 +46,23 @@ for i in search_value_range:
         # Добавляем значения ячеек на новый лист "Bad"
         bad_values = [bad_sheet.append(row) for row in ws.iter_rows(min_row=i.row, max_row=i.row, values_only=True)]
 
+    # обрабатываем флаг "None"
+    if i.value is None:
+        # Добавляем значения ячеек на новый лист "None"
+        none_values = [none_sheet.append(row) for row in ws.iter_rows(min_row=i.row, max_row=i.row, values_only=True)]
+
 # Ввод интересующей нас даты
 question = input('Введите дату: ')
 
 # Диапазон ячеек, на котором мы ищем требуемую дату поиска
 search_date_range = ws['B']
 for el in search_date_range:
-
-    # Задаём фильтр поиска по требуемой дате
-    filter_date_search = question
-    if filter_date_search in el.value:
-        # Добавляем значения ячеек на новый лист "DateChoose"
-        date_values = [date_sheet.append(row) for row in ws.iter_rows(min_row=el.row, max_row=el.row, values_only=True)]
+    if el.value is not None:
+        # Задаём фильтр поиска по требуемой дате
+        filter_date_search = question
+        if filter_date_search in el.value:
+            # Добавляем значения ячеек на новый лист "DateChoose"
+            date_values = [date_sheet.append(row) for row in ws.iter_rows(min_row=el.row, max_row=el.row, values_only=True)]
 
 # Сохраняем наши изменения
-wb.save('DAM_KEP.xlsx')
+wb.save(f'{choose_file}.xlsx')
