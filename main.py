2807cc6759aebe027a559089c361e7b60dd22abb
butERRORfly <67109808+butERRORfly@users.noreply.github.com>
2023-08-22 15:26:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ wb = openpyxl.load_workbook('DAM_KEP.xlsx')
 # Получаем доступ к активному листу
 ws = wb.active
 
+date_question = input
+
 # Фильтрация по ключам
 # Создаем новые листы
 good_sheet = wb.create_sheet('Good')
 bad_sheet = wb.create_sheet('Bad')
 none_sheet = wb.create_sheet('None')
-
+date_sheet = wb.create_sheet('DateChoose')
 
 # Копируем названия столбцов на каждый новый лист
 main_titles_values_good = [good_sheet.append(row) for row in ws.iter_rows(min_row=1, max_row=1, values_only=True)]
 main_titles_values_bad = [bad_sheet.append(row) for row in ws.iter_rows(min_row=1, max_row=1, values_only=True)]
 main_titles_values_none = [none_sheet.append(row) for row in ws.iter_rows(min_row=1, max_row=1, values_only=True)]
+main_titles_values_date = [date_sheet.append(row) for row in ws.iter_rows(min_row=1, max_row=1, values_only=True)]
 
 # Диапазон ячеек, на котором мы ищем флаги "Good", "Bad" и "None"
-search_range = ws['D2':'D2119']
-for i in search_range:
+search_value_range = ws['D2':'D2119']
+for i in search_value_range:
 
     # Обрабатываем флаг "Good"
     if i[0].value == 'Good':
@@ -30,6 +33,14 @@ for i in search_range:
     # Обрабатываем флаг "Bad"
     if i[0].value == 'Bad':
         bad_values = [bad_sheet.append(row) for row in ws.iter_rows(min_row=i[0].row, max_row=i[-1].row, values_only=True)]
+
+# Диапазон ячеек, на котром мы ищем требуемую дату поиска
+search_date_range = ws['B2':'B2119']
+for el in search_date_range:
+    filter_date_search = 'Aug 18'
+    if filter_date_search in el[0].value:
+        date_values = [date_sheet.append(row) for row in ws.iter_rows(min_row=el[0].row, max_row=el[-1].row, values_only=True)]
+
 
 # Сохраняем наши изменения
 wb.save('DAM_KEP.xlsx')
