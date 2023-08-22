e6186288f182599fd9e6d7bfe028fe3ac46d50a3
butERRORfly <67109808+butERRORfly@users.noreply.github.com>
2023-08-22 19:04:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import openpyxl
 
+
 # Получаем данные из файла
 wb = openpyxl.load_workbook('DAM_KEP.xlsx')
 
 # Получаем доступ к активному листу
 ws = wb.active
-
-date_question = input
 
 # Фильтрация по ключам
 # Создаем новые листы
@@ -22,25 +21,31 @@ main_titles_values_none = [none_sheet.append(row) for row in ws.iter_rows(min_ro
 main_titles_values_date = [date_sheet.append(row) for row in ws.iter_rows(min_row=1, max_row=1, values_only=True)]
 
 # Диапазон ячеек, на котором мы ищем флаги "Good", "Bad" и "None"
-search_value_range = ws['D2':'D2119']
+search_value_range = ws['D']
 for i in search_value_range:
 
     # Обрабатываем флаг "Good"
-    if i[0].value == 'Good':
+    if i.value == 'Good':
         # Добавляем значения ячеек на новый лист "Good"
-        good_values = [good_sheet.append(row) for row in ws.iter_rows(min_row=i[0].row, max_row=i[-1].row, values_only=True)]
+        good_values = [good_sheet.append(row) for row in ws.iter_rows(min_row=i.row, max_row=i.row, values_only=True)]
 
     # Обрабатываем флаг "Bad"
-    if i[0].value == 'Bad':
-        bad_values = [bad_sheet.append(row) for row in ws.iter_rows(min_row=i[0].row, max_row=i[-1].row, values_only=True)]
+    if i.value == 'Bad':
+        # Добавляем значения ячеек на новый лист "Bad"
+        bad_values = [bad_sheet.append(row) for row in ws.iter_rows(min_row=i.row, max_row=i.row, values_only=True)]
 
-# Диапазон ячеек, на котром мы ищем требуемую дату поиска
-search_date_range = ws['B2':'B2119']
+# Ввод интересующей нас даты
+question = input('Введите дату: ')
+
+# Диапазон ячеек, на котором мы ищем требуемую дату поиска
+search_date_range = ws['B']
 for el in search_date_range:
-    filter_date_search = 'Aug 18'
-    if filter_date_search in el[0].value:
-        date_values = [date_sheet.append(row) for row in ws.iter_rows(min_row=el[0].row, max_row=el[-1].row, values_only=True)]
 
+    # Задаём фильтр поиска по требуемой дате
+    filter_date_search = question
+    if filter_date_search in el.value:
+        # Добавляем значения ячеек на новый лист "DateChoose"
+        date_values = [date_sheet.append(row) for row in ws.iter_rows(min_row=el.row, max_row=el.row, values_only=True)]
 
 # Сохраняем наши изменения
 wb.save('DAM_KEP.xlsx')
